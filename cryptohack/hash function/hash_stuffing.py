2c37ec2a367e994ce9bc53c5c3f8618760de136e
Nguyen Tran Manh Dung <128917180+user-0069@users.noreply.github.com>
2026-05-18 10:53:27 +0700

    .

---
+++ cryptohack/hash function/hash_stuffing.py
@@ -0,0 +1,85 @@
+
+from sage.all import *
+from Crypto.Util.number import bytes_to_long, long_to_bytes
+from pwn import *
+io=remote("socket.cryptohack.org", 13405)
+import json
+context.log_level='debug'
+# 2^128 collision protection!
+BLOCK_SIZE = 32
+
+# Nothing up my sleeve numbers (ref: Dual_EC_DRBG P-256 coordinates)
+W = [0x6b17d1f2, 0xe12c4247, 0xf8bce6e5, 0x63a440f2, 0x77037d81, 0x2deb33a0, 0xf4a13945, 0xd898c296]
+X = [0x4fe342e2, 0xfe1a7f9b, 0x8ee7eb4a, 0x7c0f9e16, 0x2bce3357, 0x6b315ece, 0xcbb64068, 0x37bf51f5]
+Y = [0xc97445f4, 0x5cdef9f0, 0xd3e05e1e, 0x585fc297, 0x235b82b5, 0xbe8ff3ef, 0xca67c598, 0x52018192]
+Z = [0xb28ef557, 0xba31dfcb, 0xdd21ac46, 0xe2a91e3c, 0x304f44cb, 0x87058ada, 0x2cb81515, 0x1e610046]
+
+# Lets work with bytes instead!
+W_bytes = b''.join([x.to_bytes(4,'big') for x in W])
+X_bytes = b''.join([x.to_bytes(4,'big') for x in X])
+Y_bytes = b''.join([x.to_bytes(4,'big') for x in Y])
+Z_bytes = b''.join([x.to_bytes(4,'big') for x in Z])
+
+def pad(data):
+    padding_len = (BLOCK_SIZE - len(data)) % BLOCK_SIZE
+    return data + bytes([padding_len]*padding_len)
+
+def blocks(data):
+    return [data[i:(i+BLOCK_SIZE)] for i in range(0,len(data),BLOCK_SIZE)]
+
+def xor(a,b):
+    return bytes([x^y for x,y in zip(a,b)])
+
+def rotate_left(data, x):
+    x = x % BLOCK_SIZE
+    return data[x:] + data[:x]
+
+def rotate_right(data, x):
+    x = x % BLOCK_SIZE
+    return  data[-x:] + data[:-x]
+
+def scramble_block(block):
+    for _ in range(40):
+        block = xor(W_bytes, block)
+        block = rotate_left(block, 6)
+        block = xor(X_bytes, block)
+        block = rotate_right(block, 17)
+    return block
+
+def cryptohash(msg):
+    initial_state = xor(Y_bytes, Z_bytes)
+    msg_padded = pad(msg)
+    msg_blocks = blocks(msg_padded)
+    for i,b in enumerate(msg_blocks):
+        mix_in = scramble_block(b)
+        for _ in range(i):
+            mix_in = rotate_right(mix_in, i+11)
+            mix_in = xor(mix_in, X_bytes)
+            mix_in = rotate_left(mix_in, i+6)
+        initial_state = xor(initial_state,mix_in)
+    return initial_state.hex()
+def unscramble_block(block):
+    for _ in range(40):
+        block = rotate_left(block, 17)
+        block = xor(X_bytes, block)
+        block = rotate_right(block, 6)
+        block = xor(W_bytes, block)
+    return block
+def inv_cryptohash(digest,mix_in): #for 2 block only
+    initial_state = digest
+    initial_state =xor(initial_state, xor(Y_bytes, Z_bytes))
+    block2 =mix_in
+    block2=rotate_right(block2, 7)
+    block2=xor(block2, X_bytes)
+    block2=rotate_left(block2, 12)
+    block2= unscramble_block(block2)
+    block1 = xor(initial_state, mix_in)
+    block1 = unscramble_block(block1)
+    return block1+block2
+#just reverse, and choose the last state of mix_in!!
+pt1=b"A"*64
+pt2=inv_cryptohash(bytes.fromhex(cryptohash(pt1)),b"A"*32)
+assert cryptohash(pt1)==cryptohash(pt2)
+to_send={"m1":pt1.hex(),"m2":pt2.hex()}
+io.sendlineafter(": ",json.dumps(to_send).encode())
+print(io.recvline())
